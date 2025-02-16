be8150c3fd72bf11156e1f8f7913c2b425d6c997
arjahid <jhjahidhasanar@gmail.com>
2025-02-16 07:28:16 +0600

    added contex api

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import userIcon  from '../assets/img/user.png'
+import { AuthContext } from '../provider/AuthProvider';
 
 const Navbar = () => {
+    const {user}=useContext(AuthContext)
+    console.log('hahah',user)
     return (
         <div className='flex justify-between items-center'>
-            <div></div>
+            <div>{user.name}</div>
             <div className="nav space-x-5">
                 <Link to='/'>Home</Link>
                 <Link to='/career'>Career</Link>
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCiNgiwruj0whHWt82z15tHnZALRnn8Di0",
+  authDomain: "dragon-news-b35a9.firebaseapp.com",
+  projectId: "dragon-news-b35a9",
+  storageBucket: "dragon-news-b35a9.firebasestorage.app",
+  messagingSenderId: "294124823794",
+  appId: "1:294124823794:web:736de14e288e1692c29c6b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
